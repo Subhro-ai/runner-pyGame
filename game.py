81e11214043937078cc89f0bd7465f214243f517
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-02-02 08:54:15 +0530

    Randomise speed of snail

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from sys import exit
 
 def display_score():
@@ -6,6 +7,19 @@ def display_score():
     score_surface = test_font.render(f'Score : {current_time}', False, (64,64,64))
     score_rect = score_surface.get_rect(center = (400,50))
     screen.blit(score_surface, score_rect)
+
+def draw_fps():
+    fps = int(clock.get_fps())
+
+    # Render the text
+    fps_text = test_font.render(f"FPS: {fps}", True, (64,64,64))
+
+    # Draw the text on the screen
+    screen.blit(fps_text, (10, 10))
+        
+
+    pygame.display.update()
+    clock.tick(60)
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
@@ -21,8 +35,9 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright = (800,300))
+snailSpeed = 6
 
-player_surf = pygame.image.load('I:\Python\PyGame\graphics\Player\player_walk_1.png').convert_alpha()
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
 
 player_stand = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
@@ -30,6 +45,11 @@ player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
 player_stand_rect = player_stand.get_rect(center = (400,200))
 
 start_time = 0
+
+def randomiseSpeed(x):
+    x = random.randint(6,20)
+    return x
+
 
 player_gravity = 0
 while True:
@@ -61,8 +81,10 @@ while True:
         # screen.blit(text_surface, text_rect)
         display_score()
         screen.blit(snail_surface,snail_rect)
-        if snail_rect.right < 0: snail_rect.right = 800
-        snail_rect.right -= 4
+        if snail_rect.right < 0: 
+            snail_rect.right = 800
+            snailSpeed = randomiseSpeed(snailSpeed)
+        snail_rect.right -= snailSpeed
 
 
         #player
@@ -73,11 +95,10 @@ while True:
 
         if(player_rect.colliderect(snail_rect)):
             game_active = False
+        
+        draw_fps()
 
     else:
         screen.fill((94,129,162))
         screen.blit(player_stand ,player_stand_rect)
-        
 
-    pygame.display.update()
-    clock.tick(60)
